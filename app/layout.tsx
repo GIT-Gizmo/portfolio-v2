d2b6b75221a15458218f3422fc5271db0e0bd393
GIT-Gizmo <boluforjesus@gmail.com>
2024-07-10 18:51:04 +0100

    Added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" />
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
